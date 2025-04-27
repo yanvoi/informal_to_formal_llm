392ab7c29a69ac550592f96aa94fe9d548190d08
directtt <jedryb@st.amu.edu.pl>
2025-04-27 20:45:12 +0200

    DagsHub DVC

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -8,14 +8,22 @@ from informal_to_formal.evaluation import Evaluator
 def mock_validation_data() -> pd.DataFrame:
     return pd.DataFrame(
         {
-            'pred': ['Popełniłem błąd w raporcie.', "Nie wiem, co się stało.", "To jest test jednostkowy."],
-            'target': ['Popełniłem błąd w raporcie.', "Nwm, co się wydarzyło.", "To są testy integracyjne."],
+            "pred": [
+                "Popełniłem błąd w raporcie.",
+                "Nie wiem, co się stało.",
+                "To jest test jednostkowy.",
+            ],
+            "target": [
+                "Popełniłem błąd w raporcie.",
+                "Nwm, co się wydarzyło.",
+                "To są testy integracyjne.",
+            ],
         }
     )
 
 
 def test_evaluator_validation(mock_validation_data):
-    mock_validation_data.rename(columns={'pred': 'invalid_col_name'}, inplace=True)
+    mock_validation_data.rename(columns={"pred": "invalid_col_name"}, inplace=True)
 
     with pytest.raises(ValueError, match="Missing required column: 'pred'"):
         Evaluator(mock_validation_data)
@@ -28,7 +36,7 @@ def test_evaluate(mock_validation_data):
     assert df_all.shape[0] == mock_validation_data.shape[0]
 
     # Check if all evaluation columns are present
-    evaluation_columns = ['rouge1', 'rouge2', 'rougeL', 'bert_precision', 'bert_recall', 'bert_f1']
+    evaluation_columns = ["rouge1", "rouge2", "rougeL", "bert_precision", "bert_recall", "bert_f1"]
     assert all(col in df_all.columns for col in evaluation_columns)
 
     # Check if the average scores are returned
