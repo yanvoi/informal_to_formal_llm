14fa69b97a4a2a8aa24f766767490458be4205b8
yanvoi <janwojciechowski@op.pl>
2025-05-31 17:41:59 +0200

    janwoj: Push updated prompt

--- app/api/llm/utils/util.py
+++ app/api/llm/utils/util.py
@@ -2,6 +2,8 @@ import os
 
 
 FORMALIZE_PROMPT_PL = """
+Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
 ### Instruction:
 Przetłumacz poniższy nieformalny tekst w języku polskim na język formalny.
 
